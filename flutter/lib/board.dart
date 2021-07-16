ace60ae6316be495cb2e9949da3582c4dea3184a
Koty <koty08@naver.com>
2021-07-16 23:03:00 +0900

    07-16 삭제시 에러 처리완료, 수정 기능 구현

--- flutter/lib/board.dart
+++ flutter/lib/board.dart
@@ -9,6 +9,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 late WriteBoardState pageState;
 late ListBoardState pageState2;
 late showBoardState pageState3;
+late modifyBoardState pageState4;
 
 class WriteBoard extends StatefulWidget {
   @override
@@ -177,6 +178,7 @@ class ListBoardState extends State<ListBoard>{
             if (!snapshot.hasData) {
               return CircularProgressIndicator();
             }
+        
             return new ListView(
               children: snapshot.data!.docs.map((doc) => new ListTile(
                 title: new Text(doc['title']),
@@ -215,7 +217,6 @@ class showBoardState extends State<showBoard>{
   @override
   Widget build(BuildContext context) {
     fp = Provider.of<FirebaseProvider>(context);
-    fp.setInfo();
 
     return Scaffold(
       appBar: AppBar(title: Text("게시글 내용"),),
@@ -223,34 +224,70 @@ class showBoardState extends State<showBoard>{
         StreamBuilder(
           stream : fs.collection('posts').doc(widget.id).snapshots(),
           builder: (context, AsyncSnapshot<DocumentSnapshot>snapshot){
+            if (snapshot.hasData && !snapshot.data!.exists) {
+              return CircularProgressIndicator();
+            }
+
             if(snapshot.hasData){
-              return Column(
-                children: [
+              fp.setInfo();
+              if(fp.getInfo()['name'] == snapshot.data!['writer']){
+                return Column(
+                  children: [
                   Text(snapshot.data!['title']),
                   Text(snapshot.data!['contents']),
                   Text(snapshot.data!['writer']),
 
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.indigo[300],
+                  Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.purple[300],
+                        ),
+                        child: Text(
+                          "수정",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => modifyBoard(widget.id)));
+                          setState(() {
+                            
+                          });
+                        },
                       ),
-                      child: Text(
-                        "삭제",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        FocusScope.of(context).requestFocus(new FocusNode());
-                        fs.collection('posts').doc(widget.id).delete().then((value) => 
-                        Navigator.pop(context));
-                        setState(() {
-                        });
-                      },
                     ),
-                  ),
-                ],
-              );
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.indigo[300],
+                        ),
+                        child: Text(
+                          "삭제",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          fs.collection('posts').doc(widget.id).delete();
+                        },
+                      ),
+                    ),
+                  ],
+                  )
+                  ],
+                );
+              }
+
+              else{
+                return Column(
+                  children: [
+                  Text(snapshot.data!['title']),
+                  Text(snapshot.data!['contents']),
+                  Text(snapshot.data!['writer']),
+                  ],
+                );
+              }
             }
             return CircularProgressIndicator();
           }
@@ -258,4 +295,117 @@ class showBoardState extends State<showBoard>{
     );
   }
 
+}
+
+class modifyBoard extends StatefulWidget{
+  modifyBoard(this.id);
+  final String id;
+  @override
+  State<StatefulWidget> createState() {
+    pageState4 = modifyBoardState();
+    return pageState4;
+  }
+}
+
+class modifyBoardState extends State<modifyBoard>{
+  final FirebaseFirestore fs = FirebaseFirestore.instance;
+  late TextEditingController titleInput;
+  late TextEditingController contentInput;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(title: Text("게시물 수정")),
+      body:
+        StreamBuilder(
+        stream : fs.collection('posts').doc(widget.id).snapshots(),
+        builder: (context, AsyncSnapshot<DocumentSnapshot>snapshot) {
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return CircularProgressIndicator();
+          }
+          if(snapshot.hasData){
+            titleInput = TextEditingController(text: snapshot.data!['title']);
+            contentInput = TextEditingController(text: snapshot.data!['contents']);
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    height: 30,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                    child: TextField(
+                          controller: titleInput,
+                          decoration: InputDecoration(hintText: "제목을 입력하세요."),
+                        ),
+                    ),
+                Container(
+                    height: 50,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                    child:
+                      TextField(
+                          controller: contentInput,
+                          decoration: InputDecoration(hintText: "내용을 입력하세요."),
+                      ),
+                    ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: <Widget>[
+                //     ElevatedButton(
+                //         child: Text("카메라로 촬영하기"),
+                //         onPressed: () {
+                //           uploadImage(ImageSource.camera);
+                //         }),
+                //     ElevatedButton(
+                //         child: Text("갤러리에서 불러오기"),
+                //         onPressed: () {
+                //           uploadImage(ImageSource.gallery);
+                //         }),
+                //   ],
+                // ),
+                Divider(
+                  color: Colors.black,
+                ),
+                // Container(
+                //   child: Column(
+                //     children: <Widget>[
+                //       SizedBox(
+                //         height: 200,
+                //         width: 200,
+                //         child: Image.network(imageurl),
+                //       ),
+                //     ],
+                //   ),
+                // ),
+                Container(
+                  height: 30,
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blueAccent[200],
+                      ),
+                      child: Text(
+                        "게시물 수정",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {
+                        FocusScope.of(context).requestFocus(new FocusNode());
+                        updateOnFS(titleInput.text, contentInput.text);
+                        Navigator.pop(context);
+                      },
+                    ))
+              ],
+            );
+          }
+          return CircularProgressIndicator();
+        }
+    ));
+  }
+
+  void updateOnFS(String txt1, String txt2) async {
+    await fs.collection('posts').doc(widget.id).update({'title' : txt1,
+    'contents' : txt2});
+  }
 }
