fbf9c4e2b8c75b0db1c488929318ccb4fc41c37f
Yerin99 <diluny2@naver.com>
2021-08-23 03:33:04 +0900

    08-23 버튼 기능 추가 & 텍스트 넘어가게

--- flutter/lib/maps/place_autocomplete.dart
+++ flutter/lib/maps/place_autocomplete.dart
@@ -1,4 +1,4 @@
-// @dart=2.9
+// @dart = 2.9
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -7,6 +7,7 @@ import 'dart:async';
 
 import 'google_map_service.dart';
 import 'place.dart';
+
 
 /*
 void main() => runApp(MyApp());
@@ -32,9 +33,18 @@ class _PlaceAutocompleteState extends State<PlaceAutocomplete> {
   var uuid = Uuid();
   var sessionToken;
   var googleMapServices;
+
   PlaceDetail placeDetail;
   Completer<GoogleMapController> _controller = Completer();
   Set<Marker> _markers = Set();
+
+  var lat;
+  var lng;
+  var placeId;
+  var name;
+  var formattedAddress;
+
+  var state;
 
   @override
   void initState() {
@@ -43,9 +53,68 @@ class _PlaceAutocompleteState extends State<PlaceAutocomplete> {
         Marker(
           markerId: MarkerId('testMarker'),
           position: LatLng(35.23159301295487, 129.08395882267462),
-          infoWindow: InfoWindow(title: 'My position', snippet: 'Where am I?'),
+          infoWindow: InfoWindow(title: '여기는 어디?', snippet: '바로 부산대학교!'),
         ));
   }
+
+  void _clearMarker() {
+    if (_markers.length > 0) {
+      setState(() {
+        _markers.clear();
+      });
+    }
+  }
+
+  void _moveCamera() async {
+    state = "_moveCamera";
+    _clearMarker();
+
+    GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newLatLng(
+        LatLng(placeDetail.lat, placeDetail.lng),
+      ),
+    );
+
+    setState(() {
+      _markers.add(
+        Marker(
+            markerId: MarkerId(placeDetail.placeId),
+            position: LatLng(placeDetail.lat, placeDetail.lng),
+            infoWindow: InfoWindow(
+              title: placeDetail.name,
+              snippet: placeDetail.formattedAddress,
+            )
+        ),
+      );
+    });
+  }
+
+  void _moveCameraByButton() async {
+    state = "_moveCameraByButton";
+    _clearMarker();
+
+    GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newLatLng(
+        LatLng(lat, lng),
+      ),
+    );
+
+    setState(() {
+      _markers.add(
+        Marker(
+            markerId: MarkerId(placeId),
+            position: LatLng(lat, lng),
+            infoWindow: InfoWindow(
+              title: name,
+              snippet: formattedAddress,
+            )
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,7 +122,7 @@ class _PlaceAutocompleteState extends State<PlaceAutocomplete> {
         title: Text('Places Autocomplete'),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 32.0),
+        padding: const EdgeInsets.symmetric(horizontal: 3.0, vertical: 2.0),
         child: SingleChildScrollView(
           child: Column(
             children: <Widget> [
@@ -68,7 +137,7 @@ class _PlaceAutocompleteState extends State<PlaceAutocomplete> {
                     autofocus: true,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(),
-                        hintText: 'Search places...'
+                        hintText: '위치 찾아보기'
                     ),
                   ),
 
@@ -94,13 +163,14 @@ class _PlaceAutocompleteState extends State<PlaceAutocomplete> {
                       sessionToken,
                     );
                     sessionToken = null;
+                    _moveCamera();
                   },
                 ),
               ),
-              SizedBox(height: 10),
+              SizedBox(height: 6),
               Container(
                   width: double.infinity,
-                  height: 350,
+                  height: 450,
                   child: GoogleMap(
                     mapType: MapType.normal,
                     initialCameraPosition: CameraPosition(
@@ -114,29 +184,125 @@ class _PlaceAutocompleteState extends State<PlaceAutocomplete> {
                   )
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("블루베리 안경")),
-                  SizedBox(width: 10),
-                  ElevatedButton(onPressed: () {}, child: Text("부산은행")),
-                  SizedBox(width: 10),
-                  ElevatedButton(onPressed: () {}, child: Text("부산대역 3출")),
+                  ElevatedButton(
+                      onPressed: () => {
+                        _setPlaceToBank(),
+                        _moveCameraByButton(),
+                      },
+                      child: Text("학교 앞 부산은행")),
+                  ElevatedButton(
+                      onPressed: () => {
+                        _setPlaceToNC(),
+                        _moveCameraByButton()
+                      },
+                      child: Text("NC백화점")),
+                  ElevatedButton(
+                      onPressed: () => {
+                        _setPlaceToGS25(),
+                        _moveCameraByButton(),
+                      },
+                      child: Text("정문 원룸촌 GS")),
                 ],
               ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.blue,
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context, _searchController.text);
-                  },
-                  child: Text('여기로 결정!'))
+              Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: () => {
+                        _setPlaceToJayu(),
+                        _moveCameraByButton(),
+                      },
+                      child: Text("자유관")),
+                  ElevatedButton(
+                      onPressed: () => {
+                        _setPlaceToWoongbi(),
+                        _moveCameraByButton(),
+                      },
+                      child: Text("웅비관")),
+                  ElevatedButton(
+                      onPressed: () => {
+                        _setPlaceToJinri(),
+                        _moveCameraByButton(),
+                      },
+                      child: Text("진리관")),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.blue,
+                    ),
+                    onPressed: () {
+                      if (state == "_moveCamera") {
+                        if (placeDetail.name.length <= 9) {
+                          Navigator.pop(context, placeDetail.name);
+                        }
+                        else {
+                          Navigator.pop(context, _searchController.text);
+                        }
+                      }
+                      else if (state == "_moveCameraByButton") {
+                        Navigator.pop(context, name);
+                      }
+                    },
+                    child: Text('여기로 결정 !')),
+              )
             ],
           ),
         ),
       ),
     );
   }
+
+  void _setPlaceToNC() {
+    placeId = "ChIJVdt9bvKTaDURBtndGELj2yI";
+    formattedAddress = "대한민국 부산광역시 금정구 장전동 부산대학로63번길 2";
+    name = "부산대 NC백화점";
+    lat = 35.2322978;
+    lng = 129.0842446;
+  }
+
+  void _setPlaceToBank() {
+    placeId = "ChIJE1Y0euyTaDURgQQO8QrEw98";
+    formattedAddress = "대한민국 부산광역시 금정구 장전3동 417-36";
+    name = "학교 앞 부산은행";
+    lat = 35.2314097;
+    lng = 129.0863613;
+  }
+
+  void _setPlaceToGS25() {
+    placeId = "ChIJuSs9me6TaDUR3qt6fhMneMU";
+    formattedAddress = "대한민국 부산광역시 금정구 장전동 부산대학로64번길 60";
+    name = "GS25 장전효원점";
+    lat = 35.2340761;
+    lng = 129.0851947;
+  }
+
+  void _setPlaceToJayu() {
+    placeId = "ChIJe4dmZ0eTaDURkTqW6PYaBpc";
+    formattedAddress = "대한민국 부산광역시 금정구 장전2동 부산대학교 자유관";
+    name = "부산대학교 자유관";
+    lat = 35.2356592;
+    lng = 129.0824171;
+  }
+
+  void _setPlaceToWoongbi() {
+    placeId = "ChIJP52ZRJKTaDURr0BDIhCDtJQ";
+    formattedAddress = "대한민국 부산광역시 금정구 장전1동 부산대학교 웅비관";
+    name = "부산대학교 웅비관";
+    lat = 35.2372385;
+    lng = 129.0769734;
+  }
+
+  void _setPlaceToJinri() {
+    placeId = "ChIJaQRJOPGTaDURX1n-MDiyyB0";
+    formattedAddress = "대한민국 부산광역시 금정구 장전동 부산대학교 진리관";
+    name = "부산대학교 진리관";
+    lat = 35.238131;
+    lng = 129.0770287;
+  }
 }
+
