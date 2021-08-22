296a2619b59121239df7cdf2fc3979a839ed1f4e
joeoneul <kunyeonh0@gmail.com>
2021-08-23 03:46:58 +0900

    (08-23 대화정보창)

---
+++ flutter/lib/chat/talkinginfo.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:usdh/Widget/widget.dart';
+
+void main() => runApp(talkinginformation());
+
+class talkinginformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              topbar2(context, "채팅"),
+              imageProfile(),//TODO: 게시글제목 가져오기, 방장 프로필 불러오기(?)
+              Padding(
+                padding: EdgeInsets.fromLTRB(30, 0, 0, 10),
+              ),
+              middleDivider(),
+              Text(
+                '참여자 명단',
+                style: TextStyle(fontFamily: "SCDream",color: Color(0xff4E94EC) , fontWeight: FontWeight.w500, fontSize: 13),)
+            ],
+          ),
+        )
+    );
+  }
+}
